b0d856777d7544abe27ffd8051bdba734918d040
Keerthi Rao <kee2rao@gmail.com>
2024-12-04 14:48:38 +0530

    resolved merge conflict

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-<<<<<<< HEAD
-const { getDefaultQuizzes, getAllQuizzes , getCategories} = require('../controllers/quizController');
+const {
+  getDefaultQuizzes,
+  getAllQuizzes,
+  getCategories,
+  fetchQuizById,
+} = require('../controllers/quizController');
 const { createQuiz } = require('../controllers/quizCreation');
-=======
-const { getDefaultQuizzes, getAllQuizzes , getCategories,fetchQuizById} = require('../controllers/quizController');
 const { startQuiz, submitResponse, endQuiz } = require('../controllers/QuizStarter');
->>>>>>> 8889a30dfa56f9171325b93176027fd60d655fee
 
 const router = express.Router();
 
@@ -15,17 +16,23 @@ router.get('/default-quizzes', getDefaultQuizzes);
 // Fetch all quizzes
 router.get('/all-quizzes', getAllQuizzes);
 
-//Fetch categories
+// Fetch categories
 router.get('/categories', getCategories);
 
-<<<<<<< HEAD
+// Fetch a quiz by ID
+router.get('/quiz/:id', fetchQuizById);
+
 // Create a quiz
 router.post('/create', createQuiz);
-=======
-router.get('/:id', fetchQuizById);
-router.post('/:id/start', startQuiz); // Start a quiz
-router.post('/:id/response', submitResponse); // Submit a response
-router.post('/:id/end', endQuiz); // End a quiz and save responses
->>>>>>> 8889a30dfa56f9171325b93176027fd60d655fee
+
+// Start a quiz
+router.post('/start', startQuiz);
+
+// Submit a quiz response
+router.post('/submit', submitResponse);
+
+// End a quiz
+router.post('/end', endQuiz);
 
 module.exports = router;
+
